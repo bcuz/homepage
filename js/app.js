594567b6a7eb7379ef833b4fb2c7d13c73788409
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-24 13:40:38 -0400

    delete unnecessary code

--- js/app.js
+++ js/app.js
@@ -11,12 +11,9 @@ $(function() {
   $(window).on('scroll', function() {
     console.log($(this).scrollTop());
     if ($(this).scrollTop() > 150) {
-        // not fast...
       $("header nav").animate({backgroundColor: '#000'}, 50);
     } else {
       $("header nav").animate({backgroundColor: 'rgba(0, 0, 0, 0)'}, 50);
-      // $("header nav").removeClass("fadeIn").fadeOut().addClass(fadeOut)
-      // $("header nav").addClass("fadeOut")
     }
   })
 
