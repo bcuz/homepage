e25739c35835f5a55272ab21c4ddb24ca7e49529
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-24 08:12:49 -0400

    good start to nav background

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@
 
 $(function() {
 
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > 150) {
+      $("header nav").addClass("fadeIn")
+    } else {
+      $("header nav").removeClass("fadeIn", 2000)
+    }
+  })
+
   $('.slider').unslider({
     arrows: false,
     fluid: true
